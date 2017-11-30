d3c8f7017a6e4a880b846a9d1b9a236a026d09f2
“Martin <martin@telsmart.eu>
2017-11-30 15:36:10 +0100

    added expense summary

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = (props) => (
   <div>
     <Link to={`/edit/${props.id}`}>
       <h3>{props.description}</h3>
     </Link>
-    <p>{props.amount} - {props.createdAt}</p>
+    <p>
+      {numeral(props.amount / 100).format('$0,0.00')}
+      -
+      {moment(props.createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,7 @@
+export default (expenses) => {
+  return expenses
+    .map((expense) => expense.amount)
+    .reduce((sum, value) => {
+      return sum + value;
+    }, 0);
+};
