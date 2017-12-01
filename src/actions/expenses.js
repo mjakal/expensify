c5720b23379c8240706cbc0f55b8ae021c04ffc7
“Martin <martin@telsmart.eu>
2017-12-01 16:32:12 +0100

    CRUD working

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -53,6 +53,14 @@ export const editExpense = (id, updates) => {
   };
 };
 
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
